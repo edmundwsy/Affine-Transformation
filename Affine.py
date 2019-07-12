280991c66b4310289f1c18a0a608e383ffc7a7be
edmundwsy <edmundwsy@outlook.com>
2019-07-12 10:40:35 +0800

    Modified Rotate

--- Affine.py
+++ Affine.py
@@ -8,6 +8,32 @@ import numpy as np
 from albumentations.augmentations.transforms import DualTransform
 
 
+def rotate(img, angle, interpolation=cv2.INTER_LINEAR, border_mode=cv2.BORDER_CONSTANT, value=None, is_padding=False):
+    if is_padding:
+        h0, w0 = img.shape[:2]
+        height = w0 * math.sin(angle*math.pi/180) + h0 * math.cos(angle*math.pi/180)
+        width = h0 * math.sin(angle*math.pi/180) + w0 * math.cos(angle*math.pi/180)
+    else:
+        height, width = img.shape[:2]
+    matrix = cv2.getRotationMatrix2D((w0 // 2, h0 // 2), angle, 1.0)
+    matrix[0, 2] += abs((width- w0) / 2)
+    matrix[1, 2] += abs((height - h0) / 2)
+    img = cv2.warpAffine(img, matrix, (int(width), int(height)), flags=interpolation, borderMode=border_mode, borderValue=value)
+    return img
+
+
+def keypoint_affine(keypoint, param, rows, cols, **params):
+    # 由于不知道仿射变换对于极坐标有何影响，所以先返回0值
+    pts = np.float32([[0, 0], [cols - 1, 0], [0, rows - 1]])
+    dst = np.float32([[cols*param[0], rows*param[1]],
+                       [cols*param[2], rows*param[3]],
+                       [cols*param[3], rows*param[4]]])
+    matrix = cv2.getAffineTransform(pts, dst)
+    x, y, a, s = keypoint
+    x, y = cv2.transform(np.array([[[x, y]]]), matrix).squeeze()
+    return [x, y, 0, 0]
+
+
 def affine(img, param=[0, 0, 0, 0, 0, 0], interpolation=cv2.INTER_LINEAR, border_mode=cv2.BORDER_CONSTANT):
     h0, w0 = img.shape[:2]
     center_square = np.float32((h0, w0)) // 2
@@ -56,7 +82,7 @@ class AffineTransform(DualTransform):
     #     return F.bbox_rotate(bbox, angle, **params)
 
     def apply_to_keypoint(self, keypoint, param=[0,0,1,0,0,1], **params):
-        return F.keypoint_affine(keypoint, param, **params)
+        return keypoint_affine(keypoint, param, **params)
 
     def get_transform_init_args_names(self):
         return ('interpolation', 'border_mode')
